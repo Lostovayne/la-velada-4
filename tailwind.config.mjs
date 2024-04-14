61cf3bb1c72b9eff01e4b68637553ff1785c1bfa
Deus lo Vult <moises.barrera.dev@gmail.com>
2024-04-13 22:55:21 -0400

    agregada la extructura de la web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,12 @@ import animations from "@midudev/tailwind-animations";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#333",
+        "secondary":"#ddd"
+      }
+    },
   },
   plugins: [animations],
 };
